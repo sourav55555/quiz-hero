f5afc4d3a1a9ee64e9b23dbb14bb7fe43b6fba38
sourav55555 <souravsinghassr555@gmail.com>
2023-03-15 21:14:37 +0600

    local storage name fixed

--- index.js
+++ index.js
@@ -75,7 +75,6 @@ const displayQuiz = (data) => {
 
 // EventListener for quiz submit button
 document.querySelector("#submit").addEventListener("click", () => {
-  console.log("clicked");
   if (answers.length < 6) {
     return;
   }
@@ -109,8 +108,10 @@ document.querySelector("#submit").addEventListener("click", () => {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results"));
+  console.log("sto",storage);
   if (storage) {
+    console.log("sto1",storage);
     localStorage.setItem(
       "results",
       JSON.stringify([
@@ -123,6 +124,7 @@ document.querySelector("#submit").addEventListener("click", () => {
       ])
     );
   } else {
+    console.log("sto2",storage);
     localStorage.setItem(
       "results",
       JSON.stringify([
